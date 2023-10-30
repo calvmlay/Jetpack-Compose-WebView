8e07585a6533b849f1b5238d6f9481ede612f3b0
Calvn <mlaycalvn@gmail.com>
2023-10-30 15:55:21 +0300

    first commit

---
+++ app/src/main/java/com/chuitec/webview/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.chuitec.webview
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import com.chuitec.webview.ui.theme.WebViewTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WebViewTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    WebViewScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun WebViewScreen() {
+
+    val context = LocalContext.current
+    val webView = remember {
+        WebView(context).apply {
+            settings.javaScriptEnabled = true
+            webViewClient = WebViewClient()
+            loadUrl("https://google.com")
+        }
+    }
+    AndroidView(factory = { webView })
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WebViewPreview() {
+    WebViewTheme {
+        WebViewScreen()
+    }
+}
